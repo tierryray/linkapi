61afc2c004b6a54be25bdb2b7969882bb7fe24e2
Tierry <tierry.ray@gmail.com>
2019-12-20 17:56:14 -0300

    init first card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Container, Navbar, Card, Row, Col } from 'react-bootstrap';
 import styled from 'styled-components';
 
 const StyledNavbar = styled(Navbar)`
@@ -10,14 +10,50 @@ const Logo = styled.span`
   padding: 5px 10px;
 `;
 
+const StyledCard = styled(Card)`
+  border-top: 5px solid #b46edb;
+
+  span {
+    font-size: 18px;
+    font-weight: bold;
+    color: #b46edb;
+  }
+
+  small {
+    color: #c2c2c2;
+  }
+
+  h2 {
+    font-weight: bold;
+    margin-top: 10px;
+  }
+`;
+
 function App() {
   return (
-    <StyledNavbar variant="dark" expand="lg">
-      <Navbar.Brand href="#" className="d-flex justify-content-between">
-        <Logo>LinkAPI</Logo>
-        <Logo>LinkAPI</Logo>
-      </Navbar.Brand>
-    </StyledNavbar>
+    <>
+      <StyledNavbar variant="dark" expand="lg">
+        <Navbar.Brand href="#" className="d-flex justify-content-between">
+          <Logo>LinkAPI</Logo>
+        </Navbar.Brand>
+      </StyledNavbar>
+
+      <Container>
+        <Row>
+          <Col lg={4}>
+            <StyledCard className="shadow p-4 mt-3 bg-white rounded">
+              <span>Remaining transactions</span>
+              <small>Renew on 2019-12-31</small>
+              <h2>99,964</h2>
+
+              {/* <ContainerBar>
+                <Bar />
+              </ContainerBar> */}
+            </StyledCard>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
